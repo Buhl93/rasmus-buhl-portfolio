c2191092330f91529759534b9dbd3882c9ba124f
Rasmus Buhl <buhl.jakobsen@gmail.com>
2020-09-11 12:57:34 +0200

    Updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,8 @@ import classes from "./Header.module.css";
 export default function Header() {
   return (
     <div className={classes.HeaderText}>
-      <h1 className={classes.angle}>Rasmus Buhl</h1>
+      <h1 className={classes.Name}>Rasmus Buhl</h1>
+      <h2>Web Developer & UI/UX Designer</h2>
       <li><a href="">My Work</a></li>
       <li><a href="">About me</a></li>
       <li><a href="">Contact</a></li>
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import YatzeImg from "../../assets/images/Yatze.jpg";
+import YatzeImg from "../../assets/images/Yahtzee.PNG";
 import ShoppingImg from "../../assets/images/Shopping.jpg";
 import BurgerImg from "../../assets/images/Burger.jpg";
 
@@ -11,16 +11,21 @@ export default function MyWork() {
       <p className={classes.MyWork}>My Work</p>
       <div className={classes.Row}>
         <div>
-          <p className={classes.DescriptiveText}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-          </p>
+          <div>
+            <h3>Yahtzee Game</h3>
+            <p className={classes.DescriptiveText}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+          </div>
         </div>
-        <img src={YatzeImg} alt="Yatze" className={classes.Image} />
+        <a href="https://clever-mcnulty-472ab7.netlify.app">
+          <img src={YatzeImg} alt="Yatze" className={classes.Image} />
+        </a>
       </div>
       <div className={classes.Row}>
         <img src={ShoppingImg} alt="Shop" className={classes.Image} />
