559eb40194c551c12c72caa2dc3906a0d0e1c9c4
Rasmus Buhl <buhl.jakobsen@gmail.com>
2020-09-29 16:07:43 +0200

    up

--- src/components/AboutMeSection/AboutMe.jsx
+++ src/components/AboutMeSection/AboutMe.jsx
@@ -12,15 +12,9 @@ export default function AboutMe() {
       <div className={classes.Layout}>
         <div className={classes.Circle}></div>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+Mit navn er Rasmus, jeg er 27 år gammel og er bosat i København.
+Jeg er en ambitiøs Frontend udvikler der har fokus på eksekvering, godt design og brugervenlighed. 
+Nogle af mine projekter kan ses på denne side, jeg håber du synes om dem.
         </p>
       </div>
     </div>
